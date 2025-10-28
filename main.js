0aaff927c3d049efddb3f9b8fa4d8cac0d81bf69
greencode-dev <antonioverde.dev@gmail.com>
2025-10-28 19:59:51 +0100

    output message

--- main.js
+++ main.js
@@ -10,3 +10,15 @@ const priceForKm = 0.21;
 const basePrice = km * priceForKm;
 const discountRate = age < 18 ? 0.2 : age > 65 ? 0.4 : 0;
 let finalPrice = (basePrice * (1 - discountRate)).toFixed(2);
+
+// Output:
+// Messaggio da visualizzare
+const message = `
+Riepilogo Costo Biglietto:
+----------------
+Chilometri da percorrere: ${kmInput} km
+Età passeggero: ${ageImput} anni
+Prezzo Base: €${basePrice.toFixed(2)}
+Sconto applicato: ${discountRate * 100}%
+Prezzo Finale: €${finalPrice}
+----------------`;
